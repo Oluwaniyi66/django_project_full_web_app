092cb9cc7a5874b57bc347b23036178b7cf1f099
Oluwaniyi66 <Oluwaniyi66.oluwaniyiropo11@gmail.com>
2023-07-15 10:56:10 +0100

    added s3 and heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -146,3 +147,15 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+AWS_ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('STORAGE_BUCKET_NAME')
+
+
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_REGION_NAME = 'eu-west-2' # Your region name
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+DEFAULT_FILE_STORAGE ='storages.backends.s3boto3.S3Boto3Storage'
+AWS_QUERYSTRING_AUTH = False
--- users/models.py
+++ users/models.py
@@ -10,14 +10,14 @@ class Profile(models.Model):
 
     def __str__(self):
       return f'{self.user.username} Profile'
-
-    def save(self, *args, **kwargs):
-      super().save(*args, **kwargs)
+    #tHis is for the profile image resizing but we commentted it out to avoid ptoblem in depolyment
+    # def save(self, *args, **kwargs):
+    #   super().save(*args, **kwargs)
       
-      img = Image.open(self.image.path)
+    #   img = Image.open(self.image.path)
       
-      if img.height > 300 or img.width > 300:
-        output_size = (300, 300)
-        img.thumbnail(output_size)
-        img.save(self.image.path)
+    #   if img.height > 300 or img.width > 300:
+    #     output_size = (300, 300)
+    #     img.thumbnail(output_size)
+    #     img.save(self.image.path)
         
